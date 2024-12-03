1034aca30073ac22360e65347a337f676c225d5f
hmorrison04 <hannah.morrison@centre.edu>
2024-12-03 09:31:21 -0500

    Add files via upload

--- src/test/java/TestTree.java
+++ src/test/java/TestTree.java
@@ -11,7 +11,7 @@ public class TestTree
 	
 	Scanner comp;
 	PrintStream me;
-	boolean show = true; //if set to true, will print out respones to and from the game.  Set to false to make it faster.
+	boolean show = true; //if set to true, will print out responses to and from the game.  Set to false to make it faster.
 	
     
     //initializes the Testing environment with piped streams (Queues)
@@ -44,7 +44,7 @@ public class TestTree
     //Use this to "check" if the string from the program is correct.
 	public void check(String s)
 	{
-		String observed = comp.nextLine();
+		String observed = comp.nextLine();	
 		if(show) {System.out.println("observed:"+observed);}
 		//will not work with simple assert statements due to multithreading	
 		if( ! observed.equals(s))
@@ -68,11 +68,117 @@ public class TestTree
 		check(Strings.IS_IT_ALIVE);
         say("Y");
         //now what? Think of all the input and outputs here...
-		
+		check(Strings.IS_IT_A + Strings.DUCK + "?");
+		say("Y");
+		check(Strings.I_WIN);
+		check(Strings.PLAY_AGAIN);
+		say("Y");
+		check(Strings.IS_IT_ALIVE);
+		say("N");
+		check(Strings.IS_IT_A + Strings.ROCK + "?");
+		say("N");
+		check(Strings.WHAT_IS_THE_ANSWER);
+		say("Waterbottle");
+		check(Strings.NEW_QUESTION + Strings.ROCK + " and a Waterbottle");
+		say("Can it hold water?");
+		check("Answering yes to Can it hold water? means Waterbottle?");
+		say("Y");
+		check(Strings.THANKS);
+		check(Strings.PLAY_AGAIN);
+		say("Y");
+		check(Strings.IS_IT_ALIVE);
+		say("N");
+		check("Can it hold water?");
+		say("Y");
+		check(Strings.IS_IT_A + "Waterbottle?");
+		say("Y");
+		check(Strings.I_WIN);
+		check(Strings.PLAY_AGAIN);
+		say("Y");
+		check(Strings.IS_IT_ALIVE);
+		say("Y");
+        check(Strings.IS_IT_A + Strings.DUCK + "?");
+        say("N");
+        check(Strings.WHAT_IS_THE_ANSWER);
+        say("Dog");
+        check(Strings.NEW_QUESTION + Strings.DUCK + " and a Dog");
+        say("Does it have feathers?");
+        check("Answering yes to Does it have feathers? means Dog?");
+        say("N");
+        check(Strings.THANKS);
+        check(Strings.PLAY_AGAIN);
+        say("Y");
+        check(Strings.IS_IT_ALIVE);
+        say("Y");
+        check("Does it have feathers?");
+        say("N");
+        check(Strings.IS_IT_A + "Dog?");
+        say("Y");
+        check(Strings.I_WIN);
+        check(Strings.PLAY_AGAIN);
 
+        say("Y");
+        check(Strings.IS_IT_ALIVE);
+        say("Y");
+        check("Does it have feathers?");
+        say("N");
+        check(Strings.IS_IT_A +"Dog?");
+        say("Y");
+        check(Strings.I_WIN);
+        check(Strings.PLAY_AGAIN);
+        
+		say("Y");
+		check(Strings.IS_IT_ALIVE);
+		say("N");
+		check("Can it hold water?");
+		say("N");
+		check(Strings.IS_IT_A + Strings.ROCK + "?");
+		say("Y");
+		check(Strings.I_WIN);
+		check(Strings.PLAY_AGAIN);
+        
+//		check(Strings.PLAY_AGAIN);
+//		say("Y");
+//		check(Strings.IS_IT_ALIVE);
+//		say("N");
+//		check("Can it hold water?");
+//		say("N");
+//		check(Strings.IS_IT_A + Strings.ROCK + "?");
+//		say("N");
+//		check(Strings.WHAT_IS_THE_ANSWER);
+//		say("Shoe");
+//		check(Strings.NEW_QUESTION + Strings.ROCK + " and a Shoe");
+//		say("Does it protect feet?");
+//		check("Answering yes to Does it protect feet? means Shoe?");
+//		say("Y");
+//		check(Strings.THANKS);
+//		check(Strings.PLAY_AGAIN );
+//		say("Y");
+//		check(Strings.IS_IT_ALIVE);
+//		say("N");
+//		check("Can it hold water?");
+//		say("N");
+//		check("Does it protect feet?");
+//		say("Y");
+//		check(Strings.I_WIN);
+//		check(Strings.PLAY_AGAIN);
+
+//		say("Y");
+//		check(Strings.IS_IT_ALIVE);
+//		say("N");
+//		check("Can it hold water?");
+//		say("N");
+//		check("Does it protect feet?");
+//		say("N");
+//		check(Strings.IS_IT_A + Strings.ROCK + "?");
+//		say("Y");
+//		check(Strings.I_WIN);
+//		check(Strings.PLAY_AGAIN);
+		
+        say("N");
         
         
-        //close the streams at the end to enrue good behavior.
+        //close the streams at the end to ensure/enrue good behavior.
 		comp.close();
 		me.close();
 	}
@@ -83,7 +189,7 @@ public class TestTree
 
 	public static void main(String[] args) 
 	{
-		System.out.print("Test is running");
+		System.out.println("Test is running");
 		try
 		{
 			TestTree test = new TestTree();
