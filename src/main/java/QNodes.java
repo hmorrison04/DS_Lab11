0f435a2c64024501c1b70b12de0ef4a1ea678e2c
hmorrison04 <hannah.morrison@centre.edu>
2024-12-10 10:03:58 -0500

    Add files via upload

--- src/main/java/QNodes.java
+++ src/main/java/QNodes.java
@@ -2,19 +2,21 @@
 public class QNodes<String>
 {
 	private String val;
-	private QNodes<String> y;
-	private QNodes<String> n;
+	public QNodes<String> y;
+	public QNodes<String> n;
+	public QNodes<String> prev;
 	
 	public QNodes(String val)
 	{
-		this(val, null,null);
+		this(val, null,null,null);
 	}
 	
-	public QNodes(String val, QNodes<String> y,QNodes<String> n)
+	public QNodes(String val, QNodes<String> y,QNodes<String> n, QNodes<String> prev)
 	{
 		this.val = val;
 		this.y = y;
 		this.n = n;
+		this.prev = prev;
 	}
 	
 	public String getVal()
--- src/main/java/QTree.java
+++ src/main/java/QTree.java
@@ -8,7 +8,8 @@ public class QTree
 	
 	Scanner in;
 	PrintStream out;
-	
+	QNodes<String> curr;
+	boolean alive = true;
     //initializes the game
 	public QTree(InputStream in,PrintStream out)
 	{
@@ -22,15 +23,19 @@ public class QTree
 	public void playGame()
 	{
 		//??
-		String kP = "Y";
+		boolean kP = true;
 		String question = "a";
 		String answer;
 		
-		QNodes<String> duck = new QNodes<>(Strings.IS_IT_A + Strings.DUCK + "?");
-		QNodes<String> rock = new QNodes<>(Strings.IS_IT_A + Strings.ROCK + "?");
-		QNodes<String> root = new QNodes<>(Strings.IS_IT_ALIVE, duck,rock);
-		QNodes<String> curr = root;
-		while(kP.equals("Y"))
+		QNodes<String> duck = new QNodes<>(Strings.DUCK);
+		QNodes<String> rock = new QNodes<>(Strings.ROCK);
+		QNodes<String> root = new QNodes<>(Strings.IS_IT_ALIVE, duck,rock,null);
+		duck.prev = root;
+		rock.prev = root;
+		curr = root;
+		QNodes<String> otherSide = root;
+		
+		while(kP)
 		{
 			out.println(root.getVal());
 			answer = in.next().toLowerCase();
@@ -38,38 +43,237 @@ public class QTree
 //			{
 //				out.println(root.getYes().getVal());
 //				answer = in.next().toLowerCase();
-				traverse(answer,curr);
+				boolean isCorrect = traverse(answer,curr, otherSide);
+				if(isCorrect) 
+				{
+					out.println(Strings.I_WIN);
+					out.println(Strings.PLAY_AGAIN);
+					answer = in.next().toLowerCase();
+					if(answer.equals("y"))
+					{
+						kP = true;
+						curr = root;
+					}
+					else {
+						kP = false;
+					}
+					
+
+
+				}	
+				
+				else 
+					{
+						out.println(Strings.WHAT_IS_THE_ANSWER);
+						String realA = in.next();//.toLowerCase();
+						QNodes<String> nType = new QNodes<>(realA, null,null,null);
+						
+						out.println(Strings.NEW_QUESTION + curr.getVal() + " and a " + nType.getVal());
+						in.nextLine();
+						String qAnswer = in.nextLine();
+						
+						out.println("Answering yes to " + qAnswer + " means " + nType.getVal() + "?");
+						String a = in.next().toLowerCase();
+						QNodes<String> newQ;
+						if(a.equals("y"))
+						{
+							newQ = new QNodes<>(qAnswer,nType, curr,curr.prev);
+							if(alive) {
+								//System.out.println(alive);
+								curr.prev.y = newQ;
+							}
+							else {
+								curr.prev.n = newQ;
+							}
+							curr.prev = newQ;
+							nType.prev = newQ;//curr;
+							//newQ.prev.y = nType;
+//							curr.y = null;
+//							curr.n = null;
+						}
+						else
+						{
+						//	System.out.println(curr.prev.getVal());
+							newQ = new QNodes<>(qAnswer,curr, nType,curr.prev);
+							if(alive) {
+								curr.prev.y = newQ;
+							}
+							else {
+							curr.prev.n = newQ;
+							}
+							curr.prev = newQ;
+							nType.prev = newQ;
+							//newQ.prev.n = nType;
+//							curr.y = null;
+//							curr.n = null;
+						}
+						nType.prev = newQ;
+						curr.y = null;
+						curr.n = null;
+						out.println(Strings.THANKS);
+						
+						
+						
+						out.println(Strings.PLAY_AGAIN);
+						answer = in.next().toLowerCase();
+						if(answer.equals("y")) 
+						{
+							kP = true;
+							curr = root;
+						}
+						
+						if(answer.equals("n")) 
+						{
+							 kP = false;
+						}
+						
+					}
+				
+//				out.println(Strings.PLAY_AGAIN);
+//				answer = in.next().toLowerCase();
+//				if(answer.equals("y")) 
+//				{
+//					kP = true;
+//				}
+//				
+//				if(answer.equals("n")) 
+//				{
+//					 kP = false;
+//				}
+			
+			
 //			}
 //			else
 //			{
 //				out.println(root.getNo().getVal());
 //			}
+		alive = true;
 		}
         
 	}
-	private void traverse(String answer,QNodes<String> curr)
+	private boolean traverse(String answer,QNodes<String> curr,QNodes<String> otherSide)
 	{
+	this.curr = curr;	
+		if(curr.n == null && curr.y == null) {
+		//	System.out.println(curr.getVal());
+			return answer.equals("y");
+		}
+		
 		if(answer.equals("y"))
 		{
+			alive = true;
 			if(curr.getYes() != null)
 			{
-				curr = curr.getYes();
-				out.println(curr.getVal());
+				//otherSide = curr.getNo();
+				curr = curr.y;
+				if(curr.y == null || curr.n == null) {
+					out.println(Strings.IS_IT_A + curr.getVal() + "?");}
+				else {
+					out.println(curr.getVal());
+				}
 				answer = in.next().toLowerCase();
-				traverse(answer,curr);
+				return traverse(answer,curr,otherSide);
 			}
 		}
+		
 		if(answer.equals("n"))
 		{
+			alive = false;
+//			if(curr.getVal().equals(Strings.IS_IT_ALIVE)) {
+//			//	System.out.println("HI");
+//				alive = false;
+//			}
 			if(curr.getNo() != null)
 			{
-				curr = curr.getNo();
-				out.println(curr.getVal());
+				
+				//otherSide = curr.getYes();
+				curr = curr.n;
+				
+				if(curr.n == null || curr.y == null) 
+				{
+					out.println(Strings.IS_IT_A + curr.getVal() + "?");}
+				
+				else 
+				{
+					
+					out.println(curr.getVal());
+				}
 				answer = in.next().toLowerCase();
-				traverse(answer,curr);
+				return traverse(answer,curr,otherSide);
 			}
 		}
-	}
+	//	System.out.print(curr.n);
+		//System.out.println(curr.y);
+		
+		return false;
+	}	
+//		if(answer.equals("y"))
+//		{
+//			out.println(Strings.I_WIN);
+//			out.println(Strings.PLAY_AGAIN);
+//			answer = in.next().toLowerCase();
+//			return;
+////			if(answer.equals("y")) 
+////			{
+////				return keepPlaying = true;
+////			}
+////			
+////			if(answer.equals("n")) 
+////			{
+////				return keepPlaying = false;
+////			}
+//
+//		}
+//		
+//		else 
+//		{
+//			out.println(Strings.WHAT_IS_THE_ANSWER);
+//			String realA = in.next().toLowerCase();
+//			QNodes<String> nType = new QNodes<>(realA, null,null,null);
+//			
+//			out.println(Strings.NEW_QUESTION + curr.getVal() + " and a " + nType.getVal());
+//			in.nextLine();
+//			String qAnswer = in.nextLine();
+//			
+//			out.println("Answering yes to " + qAnswer + " means " + nType.getVal() + "?");
+//			String a = in.next().toLowerCase();
+//			QNodes<String> newQ;
+//			if(a.equals("y"))
+//			{
+//				newQ = new QNodes<>(qAnswer,nType, curr,curr.prev);
+//				curr.prev.y = newQ;
+////				curr.y = null;
+////				curr.n = null;
+//			}
+//			else
+//			{
+//				newQ = new QNodes<>(qAnswer,curr, nType,curr.prev);
+//				curr.prev.n = newQ;
+////				curr.y = null;
+////				curr.n = null;
+//			}
+//			nType.prev = newQ;
+//			curr.y = null;
+//			curr.n = null;
+//			out.println(Strings.THANKS);
+//			
+//			return;
+//			
+//			out.println(Strings.PLAY_AGAIN);
+//			answer = in.next().toLowerCase();
+//			if(answer.equals("y")) 
+//			{
+//				return keepPlaying = true;
+//			}
+//			
+//			if(answer.equals("n")) 
+//			{
+//				return keepPlaying = false;
+//			}
+			
+		//}
+		//return keepPlaying;
+	
 	
 	
 	public static void main(String[] args)
